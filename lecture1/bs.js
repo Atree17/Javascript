9c7422335aafeabcce60556dc12cbd5a7f0cbef6
Atree Verma <prabhu192004@gmail.com>
2025-07-17 12:06:43 +0000

    config

--- lecture1/bs.js
+++ lecture1/bs.js
@@ -1,1 +1,8 @@
-console.log(typeof undefined);
+let score=null;
+console.log(typeof score);
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+//strings cannot be converted shows NaN
+//""->boolean false;
+//"hi"-> boolean true;
