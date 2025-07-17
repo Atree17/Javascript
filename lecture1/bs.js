f84cdf66b5d2563ac992ee68410056c7153673ac
Atree Verma <prabhu192004@gmail.com>
2025-07-17 15:08:46 +0000

    config

--- lecture1/bs.js
+++ lecture1/bs.js
@@ -6,3 +6,16 @@ console.log(valueinNumber);
 //strings cannot be converted shows NaN
 //""->boolean false;
 //"hi"-> boolean true;
+
+let id=Symbol('123');
+let i=Symbol('123');
+console.log(id==i);
+let obj={
+    name:"hite",
+    age:32,
+}
+const ia=function(){
+    console.log("dfgdf");
+}
+ia();
+
