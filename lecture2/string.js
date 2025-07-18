68b235197e7f44ab8db06d7b7ba25658ae437cf2
Atree Verma <prabhu192004@gmail.com>
2025-07-18 17:15:01 +0000

    sg

---
+++ lecture2/string.js
@@ -0,0 +1,32 @@
+const name="hitesh"
+const repo=50
+console.log(name+repo+" Value");
+
+console.log(`dfsgdsfg ${name} ${repo}`);
+const game=new String('sgsgkjhkj');
+console.log(game.__proto__);
+console.log(game[0]);
+console.log(game.toUpperCase());
+console.log(game.length);
+console.log(game.charAt(2));
+console.log(game.indexOf('g'));
+const str=game.substring(0,4)
+const str1=game.slice(0,4);
+console.log(str);
+console.log(str1);
+const trim=new String('   sfgdfbg   ');
+console.log(trim.trim());
+const url=new String("ssdgsdg%20");
+console.log(url.replace('%20','-'));
+console.log(url.includes("hitesh"));
+const str3=new String("sfgfg-sdfgdfg-xfbdfb");
+console.log(str3.split('-'));
+
+
+
+
+
+
+
+
+
